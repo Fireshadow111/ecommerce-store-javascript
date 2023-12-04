237426c0508ce24196ee4a33494d215ce32faff3
Fireshadow111 <muhammadrajah16@gmail.com>
2023-12-04 16:21:46 +0200

    adding admin

--- script/products.js
+++ script/products.js
@@ -1,2 +1,30 @@
-let productTable = document.getElementById('productTable');
+let purchased = []
 
+let productsTable = document.querySelector('[data-productsTable]')
+
+
+let products = JSON.parse(localStorage.getItem('products'))
+
+
+function displayProducts() {
+
+    let dProducts = products.map(function(item, index) {
+
+        return `
+        
+        <tr>
+        <td><img src ='${item.url}'</td>
+        <td style = "color: #44d62c">${item.name}</td>
+        <td style = "color: #44d62c">${item.quantity}</td>
+        <td style = "color: #44d62c">${item.description}</td>
+        <td style = "color: #44d62c">${item.price}</td>
+        <td><button class = "addCartButton">Add to Cart</button></td>
+        </tr>
+        `
+    
+
+})
+productsTable.innerHTML = dProducts.join('')
+
+}
+displayProducts()
