ba6d92ee289bc52afa0764978c12ab081f9be28e
Fireshadow111 <muhammadrajah16@gmail.com>
2023-12-05 03:22:31 +0200

    placing products in cards

--- script/products.js
+++ script/products.js
@@ -1,6 +1,6 @@
 let purchased = []
 
-let productsTable = document.querySelector('[data-productsTable]')
+let productsTable = document.querySelector('[data-productsCard]')
 
 
 let products = JSON.parse(localStorage.getItem('products'))
@@ -11,15 +11,18 @@ function displayProducts() {
     let dProducts = products.map(function(item, index) {
 
         return `
-        
-        <tr>
-        <td><img src ='${item.url}'</td>
-        <td style = "color: #44d62c">${item.name}</td>
-        <td style = "color: #44d62c">${item.quantity}</td>
-        <td style = "color: #44d62c">${item.description}</td>
-        <td style = "color: #44d62c">${item.price}</td>
-        <td><button class = "addCartButton">Add to Cart</button></td>
-        </tr>
+        <div class="col-md-4 my-5">
+        <div class="card" style="width: 18rem;">
+        <img src="${item.url}" class="card-img-top" alt="">
+        <div class="card-body">
+            <h5 class="card-title">${item.name}</h5>
+            <p class="card-text">${item.description}</p>
+            <p class="card-text">Quantity: ${item.quantity}</p>
+            <p class="card-text" style="color: #44d62c;">Price: ${item.price}</p>
+            <button class="btn btn-primary addCartButton">Add to Cart</button>
+        </div>
+    </div>
+    </div>
         `
     
 
@@ -28,3 +31,33 @@ productsTable.innerHTML = dProducts.join('')
 
 }
 displayProducts()
+
+
+
+
+
+
+
+
+// function displayProducts() {
+
+//     let dProducts = products.map(function(item, index) {
+
+//         return `
+        
+//         <tr>
+//         <td><img width = "500px" height = "350px" src ='${item.url}'</td>
+//         <td class = "tableData" style = "color: #44d62c">${item.name}</td>
+//         <td class style = "color: #44d62c">${item.quantity}</td>
+//         <td style = "color: #44d62c">${item.description}</td>
+//         <td style = "color: #44d62c">${item.price}</td>
+//         <td><button class = "addCartButton">Add to Cart</button></td>
+//         </tr>
+//         `
+    
+
+// })
+// productsTable.innerHTML = dProducts.join('')
+
+// }
+// displayProducts()
